97f1aa301df4ab77b4a1e86aadb10d152ce34c64
DRACula508301 <112517349+DRACula508301@users.noreply.github.com>
2024-05-24 11:51:05 -0500

    Delete dataset/config.py

--- dataset/config.py
+++
@@ -1,2 +0,0 @@
-FGR_PATH = 'C:\\Users\\DRACula\\Box\\In Utero Wellcome Leap Project\\FGR Study Ultrasound Data\\Placenta Data\\Analysis\\FGR_Patients_Segmented'
-CONTROLLED_PATH = 'C:\\Users\\DRACula\\Box\\In Utero Wellcome Leap Project\\FGR Study Ultrasound Data\\Placenta Data\\Analysis\\Control_Patients_Segmented'
